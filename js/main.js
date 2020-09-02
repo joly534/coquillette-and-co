3bd92aa5b97066a874fd887558447edad7802c7b
jonathan joly <joly534@gmail.com>
2020-09-02 23:15:50 +0200

    update

--- js/main.js
+++ js/main.js
@@ -5,27 +5,27 @@ var valueTagliatelle = 0;
 function produceTagliatelle()
 {
     var state = false;
+    var pourcentageTagliatelle = 0;
+    /******div qui entoure la barre de progression******/
     var tagliatelle = document.getElementById('taglia')
+    /******div de la barre de  progression******/
     var avancement = document.getElementById('avancement-tagliatelle');
-    avancement.style.width = 0 + '%';
-    if (state == false) {
-        avancement.style.width = 0 + '%';
+    /******unite de mesure */
+    var poids = 'gr';
+        
         setInterval(() => {
-            valueTagliatelle += 10;
-            tagliatelle.innerHTML= "";
-            tagliatelle.innerHTML= "Quantité : " +  valueTagliatelle + " grammes";
-            avancement.style.width = 100 + '%';
-            console.log(tagliatelle);
-        }, 2500);
-    } else {
-        setTimeout(() => {
-            valueTagliatelle += 10;
-            tagliatelle.innerHTML= "";
-            tagliatelle.innerHTML= "Quantité : " + valueTagliatelle + " grammes";
-            avancement.style.width = 100 + '%';
-            console.log(tagliatelle);
-        }, 2500);
-    }
+            pourcentageTagliatelle += 1;
+            avancement.innerHTML = pourcentageTagliatelle + '%';
+            avancement.style.width = pourcentageTagliatelle + '%';
+            if (pourcentageTagliatelle == 100) {
+                valueTagliatelle += 100;
+                pourcentageTagliatelle = 0;
+                tagliatelle.innerHTML= "";
+                tagliatelle.innerHTML= "Quantité : " +  valueTagliatelle + poids;
+            } else if ((valueTagliatelle >= 1000) && (valueTagliatelle < 1000000)) { poids = 'kg';
+            } else if ((valueTagliatelle >= 1000000) && (valueTagliatelle < 2000000)) { poids = ' tonne';
+            } else if (valueTagliatelle >= 2000000) { poids = ' tonnes'; }      
+        }, 16);
 }
 
 function update(){
