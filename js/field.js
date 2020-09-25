cfe00a2d9287ef5b8ecd47570e8d80ddb1ee7416
jonathan joly <joly534@gmail.com>
2020-09-25 15:34:06 +0200

    update

--- js/field.js
+++ js/field.js
@@ -1,4 +1,4 @@
-var buttonFlour= new ButtonOfGame ('field','farine','Farine',valueNumberOfWorker,'assets/pictures/production/flour.jpg',50,'produce',1,' Kg');
+var buttonFlour = new ButtonOfGame ('field','farine','Farine',valueNumberOfWorker,'assets/pictures/production/flour.jpg',50,'produce',1,' Kg');
 var buttonEgg = new ButtonOfGame('field','oeuf','Oeufs',valueNumberOfWorker,'assets/pictures/production/egg.jpg',55,'produce',2,' Unités');
 var buttonWatter = new ButtonOfGame('field','eau','Eau',valueNumberOfWorker,'assets/pictures/production/water.jpg',45,'produce',1, 'Unités');
 buttonFlour.draw();
--- js/shop.js
+++ js/shop.js
@@ -1,1 +1,7 @@
-var buttonMarche = new ButtonOfGame('shop','marche','Marché',valueNumberOfWorker,)
+var buttonMarche = new ButtonOfGame('shop','marche','Marché',valueNumberOfWorker,'assets/pictures/magasin/vendeur.jpg',100,'sale',1,'Kg');
+var buttonStore = new ButtonOfGame('shop','magasin','Magasin',valueNumberOfWorker,'assets/pictures/magasin/store.jpg',100,'sale',1,'Kg');
+var buttonrestaurant = new ButtonOfGame('shop','restaurant','Restaurant',valueNumberOfWorker,'assets/pictures/magasin/restaurant.jpg',100,'sale',1,'Kg');
+
+buttonMarche.draw();
+buttonStore.draw();
+buttonrestaurant.draw();
